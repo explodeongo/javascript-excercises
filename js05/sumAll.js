c592f6425f326258345654aca11cca875dd9c606
explodeongo <arnipvan2004@gmail.com>
2024-03-12 16:40:51 +0530

    FIFTH

---
+++ js05/sumAll.js
@@ -0,0 +1,13 @@
+const sumAll = function (x, y) {
+  if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+  if (x < 0 || y < 0) return "ERROR";
+  if (x > y) [x, y] = [y, x];
+  let sum = 0;
+  for (let i = x; i <= y; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+// Do not edit below this line
+module.exports = sumAll;
