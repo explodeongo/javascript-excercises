beb164b80474daff7b1ea051157f14d317dd8e35
explodeongo <arnipvan2004@gmail.com>
2024-03-12 14:06:33 +0530

    first

---
+++ js01/helloWorld.spec.js
@@ -0,0 +1,7 @@
+import helloWorld from "./helloWorld";
+
+describe("Hello World", function () {
+  test('says "Hello, World!"', function () {
+    expect(helloWorld()).toEqual("Hello, World!");
+  });
+});
