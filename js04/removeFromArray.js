496f5bfad6d0cb26a56dd3be01e5e8e62e4512e4
explodeongo <arnipvan2004@gmail.com>
2024-03-12 16:29:33 +0530

    Fourth

---
+++ js04/removeFromArray.js
@@ -0,0 +1,12 @@
+const removeFromArray = function (array, ...args) {
+  const newArray = [];
+
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+
+  return newArray;
+};
+module.exports = removeFromArray;
