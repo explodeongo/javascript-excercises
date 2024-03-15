923d32d35aa2b4868e8233f332e6b2bdd29f7a3b
explodeongo <arnipvan2004@gmail.com>
2024-03-15 09:45:17 +0530

    did the 6th one!

---
+++ js06/leapYears.js
@@ -0,0 +1,10 @@
+const leapYears = function (x) {
+  if (x % 4 === 0 && (x % 100 !== 0 || x % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Do not edit below this line
+module.exports = leapYears;
